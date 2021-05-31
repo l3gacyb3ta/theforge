b03e735ce2aae94eef52bdb5a989f78fcf7ee693
Gitea <l3gacy.b3ta@gmail.com>
2021-05-31 13:52:56 -0400

    better splitbee integration

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,8 @@
 import getPostData from '../lib/airtable'
+import splitbee from '@splitbee/web'
 
 export default function Home({ html }) {
+  splitbee.init()
 
   return (
     <>
@@ -83,7 +85,7 @@ async function genHTML() {
   
   <!--[if lt IE 9]><script src="https://cdnjs.cloudflare.com/ajax/libs/html5shiv/3.7.3/html5shiv.js"></script><![endif]-->
   <!--[if lt IE 9]><script src="js/respond.js"></script><![endif]-->
-  `+ data.analytics.value + `
+  
   </head>
   
   <body class="hidden-bar-wrapper">
